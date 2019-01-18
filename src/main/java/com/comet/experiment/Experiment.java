8a14114256475ac1f5c8f1b266af5e202693448e
chrisroberts <chris@comet.ml>
2019-01-18 16:45:16 -0500

    [ch2938] remove extra log line

--- src/main/java/com/comet/experiment/Experiment.java
+++ src/main/java/com/comet/experiment/Experiment.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Optional;
-import java.util.logging.Level;
 
 public class Experiment {
     private Connection connection;
