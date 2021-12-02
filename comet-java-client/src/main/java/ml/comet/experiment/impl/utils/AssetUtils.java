265e27799af41ba0c793714ba5e2f76144bb634d
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2021-12-02 20:17:32 +0200

    CM-1630: Implemented utility method to create RemoteAsset. Implemented related test cases.

--- comet-java-client/src/main/java/ml/comet/experiment/impl/utils/AssetUtils.java
+++ comet-java-client/src/main/java/ml/comet/experiment/impl/utils/AssetUtils.java
@@ -2,11 +2,15 @@ package ml.comet.experiment.impl.utils;
 
 import lombok.experimental.UtilityClass;
 import ml.comet.experiment.impl.asset.Asset;
+import ml.comet.experiment.impl.asset.RemoteAsset;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.stream.Stream;
 
 /**
@@ -14,6 +18,8 @@ import java.util.stream.Stream;
  */
 @UtilityClass
 public class AssetUtils {
+
+    public static final String REMOTE_FILE_NAME_DEFAULT = "remote";
 
     /**
      * Walks through the asset files in the given folder and produce stream of {@link Asset} objects holding information
@@ -34,6 +40,45 @@ public class AssetUtils {
         // list files in the directory and process each file as an asset
         return FileUtils.listFiles(folder, recursive)
                 .map(path -> mapToFileAsset(folder, path, logFilePath, prefixWithFolderName));
+    }
+
+    /**
+     * Creates remote asset representation encapsulated into {@link RemoteAsset} instance.
+     *
+     * @param uri       the {@link URI} pointing to the remote asset location. There is no imposed format,
+     *                  and it could be a private link.
+     * @param fileName  the optional "name" of the remote asset, could be a dataset name, a model file name.
+     * @param overwrite if {@code true} will overwrite all existing assets with the same name.
+     * @param metadata  Some additional data to attach to the remote asset.
+     *                  The dictionary values must be JSON compatible.
+     * @return the initialized {@link RemoteAsset} instance.
+     */
+    public static RemoteAsset createRemoteAsset(URI uri, String fileName, boolean overwrite,
+                                                Map<String, Object> metadata) {
+        RemoteAsset asset = new RemoteAsset();
+        asset.setLink(uri);
+        asset.setOverwrite(overwrite);
+        asset.setMetadata(metadata);
+        if (StringUtils.isBlank(fileName)) {
+            asset.setFileName(remoteAssetFileName(uri));
+        } else {
+            asset.setFileName(fileName);
+        }
+        return asset;
+    }
+
+    static String remoteAssetFileName(URI uri) {
+        String fileName = uri.getPath();
+        if (StringUtils.isBlank(fileName)) {
+            return REMOTE_FILE_NAME_DEFAULT;
+        }
+        // get last portion of the name
+        int lastSlashIndex = fileName.lastIndexOf("/");
+        if (lastSlashIndex >= 0) {
+            return fileName.substring(lastSlashIndex + 1);
+        } else {
+            return fileName;
+        }
     }
 
     static Asset mapToFileAsset(File folder, Path assetPath,
