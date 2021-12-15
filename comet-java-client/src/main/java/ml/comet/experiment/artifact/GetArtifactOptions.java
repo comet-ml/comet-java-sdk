e3782b2facc7753d1f7559dc7b441c368e009d7d
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2021-12-15 13:06:47 +0200

    CM-1631: Implemented GetArtifactOptionsBuilder factory to create properly initialized instance of the GetArtifactOptions.

--- comet-java-client/src/main/java/ml/comet/experiment/artifact/GetArtifactOptions.java
+++ comet-java-client/src/main/java/ml/comet/experiment/artifact/GetArtifactOptions.java
@@ -23,111 +23,132 @@ public final class GetArtifactOptions {
     }
 
     /**
-     * Factory to create default empty options.
+     * Factory to create {@link GetArtifactOptionsBuilder} which can be used to create properly initialized
+     * instance of the {@link GetArtifactOptions}.
      *
-     * @return the initialized {@link GetArtifactOptions} instance.
+     * @return the initialized {@link GetArtifactOptionsBuilder} instance.
      */
     @SuppressWarnings("checkstyle:MethodName")
-    public static GetArtifactOptions Op() {
-        return new GetArtifactOptions();
+    public static GetArtifactOptionsBuilder Op() {
+        return new GetArtifactOptionsBuilder();
     }
 
     /**
-     * Creates option with workspace name/id.
-     *
-     * @param workspace the workspace name/id.
-     * @return the {@link GetArtifactOptions} instance with option set.
+     * The builder to create properly initialized instances of the {@link GetArtifactOptions}.
      */
-    public GetArtifactOptions workspaceName(String workspace) {
-        this.workspace = workspace;
-        return this;
-    }
+    static final class GetArtifactOptionsBuilder {
+        final GetArtifactOptions options;
 
-    /**
-     * Creates option with project name/id.
-     *
-     * @param project the project name/id.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions projectName(String project) {
-        this.project = project;
-        return this;
-    }
+        GetArtifactOptionsBuilder() {
+            this.options = new GetArtifactOptions();
+        }
 
-    /**
-     * Creates option with artifact name.
-     *
-     * @param artifactName the artifact name.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions name(String artifactName) {
-        this.artifactName = artifactName;
-        return this;
-    }
+        /**
+         * Creates option with workspace name/id.
+         *
+         * @param workspace the workspace name/id.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder workspaceName(String workspace) {
+            this.options.workspace = workspace;
+            return this;
+        }
 
-    /**
-     * Creates option with artifact ID.
-     *
-     * @param artifactId the ID of the artifact.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions artifactId(String artifactId) {
-        this.artifactId = artifactId;
-        return this;
-    }
+        /**
+         * Creates option with project name/id.
+         *
+         * @param project the project name/id.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder projectName(String project) {
+            this.options.project = project;
+            return this;
+        }
 
-    /**
-     * Creates option with specific version of the artifact to be retrieved.
-     *
-     * @param version the version of the artifact.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions version(String version) {
-        this.version = version;
-        return this;
-    }
+        /**
+         * Creates option with artifact name.
+         *
+         * @param artifactName the artifact name.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder name(String artifactName) {
+            this.options.artifactName = artifactName;
+            return this;
+        }
 
-    /**
-     * Creates option with specific ID of the version of the artifact to be retrieved.
-     *
-     * @param versionId the ID of specific version of the artifact.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions versionId(String versionId) {
-        this.versionId = versionId;
-        return this;
-    }
+        /**
+         * Creates option with artifact ID.
+         *
+         * @param artifactId the ID of the artifact.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder artifactId(String artifactId) {
+            this.options.artifactId = artifactId;
+            return this;
+        }
 
-    /**
-     * Creates option to get artifact version with specific alias.
-     *
-     * @param alias the artifact version alias.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions alias(String alias) {
-        this.alias = alias;
-        return this;
-    }
+        /**
+         * Creates option with specific version of the artifact to be retrieved.
+         *
+         * @param version the version of the artifact.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder version(String version) {
+            this.options.version = version;
+            return this;
+        }
 
-    /**
-     * Creates option to get artifact version either by alias or version string.
-     *
-     * @param versionOrAlias the alias or version string.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions versionOrAlias(String versionOrAlias) {
-        this.versionOrAlias = versionOrAlias;
-        return this;
-    }
+        /**
+         * Creates option with specific ID of the version of the artifact to be retrieved.
+         *
+         * @param versionId the ID of specific version of the artifact.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder versionId(String versionId) {
+            this.options.versionId = versionId;
+            return this;
+        }
 
-    /**
-     * Creates option allowing to override the experiment key associated with artifact.
-     *
-     * @param consumerExperimentKey the new experiment key to be associated.
-     * @return the {@link GetArtifactOptions} instance with option set.
-     */
-    public GetArtifactOptions consumerExperimentKey(String consumerExperimentKey) {
-        this.consumerExperimentKey = consumerExperimentKey;
-        return this;
+        /**
+         * Creates option to get artifact version with specific alias.
+         *
+         * @param alias the artifact version alias.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder alias(String alias) {
+            this.options.alias = alias;
+            return this;
+        }
+
+        /**
+         * Creates option to get artifact version either by alias or version string.
+         *
+         * @param versionOrAlias the alias or version string.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder versionOrAlias(String versionOrAlias) {
+            this.options.versionOrAlias = versionOrAlias;
+            return this;
+        }
+
+        /**
+         * Creates option allowing to override the experiment key associated with artifact.
+         *
+         * @param consumerExperimentKey the new experiment key to be associated.
+         * @return the {@link GetArtifactOptionsBuilder} instance with option set.
+         */
+        public GetArtifactOptionsBuilder consumerExperimentKey(String consumerExperimentKey) {
+            this.options.consumerExperimentKey = consumerExperimentKey;
+            return this;
+        }
+
+        /**
+         * Creates properly initialized instance of the {@link GetArtifactOptions}.
+         *
+         * @return the properly initialized instance of the {@link GetArtifactOptions}.
+         */
+        public GetArtifactOptions build() {
+            return this.options;
+        }
     }
 }
