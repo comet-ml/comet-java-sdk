3225a7e7d5b8a714d2d6b73ea33c8282f307f387
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2021-12-31 14:45:32 +0200

    CM-1632: Removed obsolete import.

--- comet-java-client/src/main/java/ml/comet/experiment/impl/http/Connection.java
+++ comet-java-client/src/main/java/ml/comet/experiment/impl/http/Connection.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
