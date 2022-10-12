d1f5acba06207876fee6067ef43cbe5d26bd153b
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2022-10-12 17:46:32 +0300

    CM-2347: Implemented CometApi.getRegistryModelsCount() and related test cases.

--- comet-java-client/src/main/java/ml/comet/experiment/impl/constants/ApiEndpoints.java
+++ comet-java-client/src/main/java/ml/comet/experiment/impl/constants/ApiEndpoints.java
@@ -53,4 +53,6 @@ public final class ApiEndpoints {
     public static final String DOWNLOAD_REGISTRY_MODEL = READ_API_URL + "/registry-model/item/download";
     public static final String GET_REGISTRY_MODEL_DETAILS = READ_API_URL + "/registry-model/details";
     public static final String GET_REGISTRY_MODEL_NOTES = READ_API_URL + "/registry-model/notes";
+
+    public static final String GET_REGISTRY_MODELS_COUNT = READ_API_URL + "/registry-model/count";
 }
